d6e4304ddc684c54e0f9995fab494215de1c0e17
mohamed <m635379@gmail.com>
2023-12-31 10:49:35 +0200

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import FileHighlighter from "./components/test";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      {/* <FileUploadButton setIsOpen={setIsOpen} />
+      <FileUploadButton setIsOpen={setIsOpen} />
       <FileUploadMenu isOpen={isOpen} setIsOpen={setIsOpen} />
-      <ImageEditor /> */}
-      <FileHighlighter />
     </>
   );
 }
